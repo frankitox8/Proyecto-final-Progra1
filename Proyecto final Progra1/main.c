512422bc5c04329b29c8a93be4adc0532f504f25
Franco Ferreira <fredbear8776@gmail.com>
2026-06-05 15:26:05 -0300

    Agregué Structs al main

--- Proyecto final Progra1/main.c
+++ Proyecto final Progra1/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define DIMTEXTO 30
+typedef struct{
+int id;
+char nombre[DIMTEXTO];
+float precio;
+
+}stProducto
+
+typedef struct{
+int id;
+char categoria[DIMTEXTO];
+}stTransporte
+
+typedef struct{
+    int id;
+    int dni;
+    char nombre[DIMTEXTO];
+    int edad;
+    char puesto[DIMTEXTO];
+}stEmpleado
+
+typedef struct{
+    int id;
+    int dni;
+    char nombre[DIMTEXTO];
+    stProducto carrito[DIMTEXTO];
+}stCliente
 
 int main()
 {
-    printf("Hello world!\n");
+
+
     return 0;
 }
+
+
