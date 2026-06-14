b6f94848fe56a849feecce725290e0523d0ece5c
Mateo Bravo <mateobravo552@gmail.com>
2026-06-14 19:31:20 -0300

    Update main.c

--- Proyecto final Progra1/main.c
+++ Proyecto final Progra1/main.c
@@ -28,11 +28,29 @@ typedef struct{
     stProducto carrito[DIMTEXTO];
 }stCliente
 
+void menu();
+
 int main()
 {
-
-
+    menu();
     return 0;
 }
 
-
+void menu()
+{
+    int op = 0;
+    do
+    {
+        printf("\nIngrese la opcion: \n");
+        printf("_________________________________\n");
+        printf("| [1] |\n")
+        printf("| [2] |\n")
+        printf("| [3] |\n")
+        printf("| [4] |\n")
+        printf("| [5] |\n")
+        printf("| [6] |\n")
+        printf("| [7] |\n")
+        printf("| [0] FINALIZAR EL PROGRAMA     |\n");
+        printf("---------------------------------");
+    }while(op != 0);
+}
